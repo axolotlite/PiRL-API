be10466a4e8129b043b2ff26db3e6e6fb6e5f3e6
Mohamed Amr <mohamedamrfouad19@gmail.com>
2023-07-01 17:22:07 +0300

    added modified api calls

--- pirl_api.py
+++ pirl_api.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import base64
+from fastapi.responses import FileResponse
+from fastapi.param_functions import Path
 import os
 import cv2
 import numpy
@@ -34,21 +36,80 @@ class APIWrapper():
         self.db_handler = DBHandler()
         # self.instance = ThreadedUvicorn(config)
     def configure_routes(self):
-        @self.app.get("/list-directory")
+    #     @self.app.get("/list-directory")
+    #     async def list_directory():
+    #         available_directories = {
+    #             "material": [], # lecture material
+    #             "transcripts": [], # Ai based lecture transcript
+    #             "summaries": [], # Ai summerization of transcripts
+    #             "videos": [] # the recorded video
+    #         }
+    #         files = []
+    #         for directory in available_directories:
+    #             try:
+    #                 available_directories[directory] ={"content":os.listdir("data/" + directory)}
+    #             except FileNotFoundError:
+    #                 return {"message": "error: recreate directories"}
+    #         return json.dumps(available_directories)
+
+
+        @self.app.get("/list-directory/material")
         async def list_directory():
-            available_directories = {
-                "material": [], # lecture material
-                "transcripts": [], # Ai based lecture transcript
-                "summaries": [], # Ai summerization of transcripts
-                "videos": [] # the recorded video
-            }
             files = []
-            for directory in available_directories:
+
+            for file in os.listdir("data\material"):
                 try:
-                    available_directories[directory] ={"content":os.listdir("data/" + directory)}
+                    files.append(file)
+                    # available_directories[directory] ={"content":os.listdir("data/" + directory)}
                 except FileNotFoundError:
                     return {"message": "error: recreate directories"}
-            return json.dumps(available_directories)
+            print(files)
+            print(list(map(lambda file: file, files)))
+            return list(map(lambda file: file, files))
+        
+        @self.app.get("/list-directory/summaries")
+        async def list_directory():
+            files = []
+
+            for file in os.listdir("data\summaries"):
+                try:
+                    files.append(file)
+                    # available_directories[directory] ={"content":os.listdir("data/" + directory)}
+                except FileNotFoundError:
+                    return {"message": "error: recreate directories"}
+            print(files)
+            print(list(map(lambda file: file, files)))
+            return list(map(lambda file: file, files))
+        
+        @self.app.get("/list-directory/transcripts")
+        async def list_directory():
+            files = []
+
+            for file in os.listdir("data/transcripts"):
+                try:
+                    files.append(file)
+                    # available_directories[directory] ={"content":os.listdir("data/" + directory)}
+                except FileNotFoundError:
+                    return {"message": "error: recreate directories"}
+            print(files)
+            print(list(map(lambda file: file, files)))
+            return list(map(lambda file: file, files))
+        
+        @self.app.get("/list-directory/videos")
+        async def list_directory():
+            files = []
+
+            for file in os.listdir("data/videos"):
+                try:
+                    files.append(file)
+                    # available_directories[directory] ={"content":os.listdir("data/" + directory)}
+                except FileNotFoundError:
+                    return {"message": "error: recreate directories"}
+            print(files)
+            print(list(map(lambda file: file, files)))
+            return list(map(lambda file: file, files))
+
+
         @self.app.post("/attend/")
         async def attend(attendance: Attendance):
             #attendence in db
@@ -59,16 +120,60 @@ class APIWrapper():
             print(attendance)
             return {"message": f"{attendance.user} attendance for {attendance.lessonId} recorded"}
 
-        @self.app.get("/download")
-        def download_file(directory: str, file_name: str):
-            file_path = "./data/" + directory + '/' + file_name
-            if not os.path.exists(file_path):
-                print(file_path)
-                raise HTTPException(status_code=404, detail="File not found")
-            def file_generator():
-                with open(file_path, "rb") as file:
-                    yield from file
-            return StreamingResponse(content=file_generator(), media_type="application/pdf")
+        # @self.app.get("/download")
+        # def download_file(directory: str, file_name: str):
+        #     file_path = "./data/" + directory + '/' + file_name
+        #     if not os.path.exists(file_path):
+        #         print(file_path)
+        #         raise HTTPException(status_code=404, detail="File not found")
+        #     def file_generator():
+        #         with open(file_path, "rb") as file:
+        #             yield from file
+        #     return StreamingResponse(content=file_generator(), media_type="application/pdf")
+
+        @self.app.get('/material/{filename}')
+        async def download_file(filename: str = Path(...)):
+            directory = 'data/material'  # Set the directory where the files are stored
+            print("hi i've been called material download")
+            # Validate the requested file against a whitelist if needed
+            # whitelist = ['file1.txt', 'file2.pdf']
+            # if filename not in whitelist:
+            #     raise HTTPException(status_code=404, detail='File not found')
+
+            return FileResponse(f'{directory}/{filename}', filename=filename, media_type='application/octet-stream')
+        
+        @self.app.get('/summaries/{filename}')
+        async def download_file(filename: str = Path(...)):
+            directory = 'data/summaries'  # Set the directory where the files are stored
+            print("hi i've been called summaries download")
+            # Validate the requested file against a whitelist if needed
+            # whitelist = ['file1.txt', 'file2.pdf']
+            # if filename not in whitelist:
+            #     raise HTTPException(status_code=404, detail='File not found')
+
+            return FileResponse(f'{directory}/{filename}', filename=filename, media_type='application/octet-stream')
+        
+        @self.app.get('/transcripts/{filename}')
+        async def download_file(filename: str = Path(...)):
+            directory = 'data/transcripts'  # Set the directory where the files are stored
+            print("hi i've been called transcripts download")
+            # Validate the requested file against a whitelist if needed
+            # whitelist = ['file1.txt', 'file2.pdf']
+            # if filename not in whitelist:
+            #     raise HTTPException(status_code=404, detail='File not found')
+
+            return FileResponse(f'{directory}/{filename}', filename=filename, media_type='application/octet-stream')
+        
+        @self.app.get('/videos/{filename}')
+        async def download_file(filename: str = Path(...)):
+            directory = 'data/videos'  # Set the directory where the files are stored
+            print("hi i've been called videos download")
+            # Validate the requested file against a whitelist if needed
+            # whitelist = ['file1.txt', 'file2.pdf']
+            # if filename not in whitelist:
+            #     raise HTTPException(status_code=404, detail='File not found')
+
+            return FileResponse(f'{directory}/{filename}', filename=filename, media_type='application/octet-stream')
 
         @self.app.get("/OK")
         def health_check():
